32ab5eb7e96e6b2c29d48cd9c6d159a2f794b9d7
Aswin Gopinathan <40236624+infiniteoverflow@users.noreply.github.com>
2023-02-16 20:49:03 +0530

    Refactored the Collection section

--- grocer_store/ProductListingCollectionView/Views/ProductCollectionViewCell.swift
+++ grocer_store/ProductListingCollectionView/Views/ProductCollectionViewCell.swift
@@ -9,4 +9,110 @@ import UIKit
 
 class ProductCollectionViewCell: UICollectionViewCell {
     
+    /// Unique identifier for the TableView Cell
+    static let identifier = "ProductCollectionViewCell"
+    
+    /// Store Item
+    var item: Item? {
+        didSet {
+            itemNameLabel.text = item?.name
+            itemPriceLabel.text = item?.price
+            
+            guard let imageUrl = item?.image else {
+                return
+            }
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: URL(string: imageUrl)!) {
+                    if let image = UIImage(data: data) {
+                        Task {
+                            self?.itemImageView.image = image
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    /// Shows the image of the item
+    let itemImageView : UIImageView = {
+        let ui = UIImageView()
+        return ui
+    }()
+    
+    /// Shows the name of the item
+    let itemNameLabel: UILabel = {
+        let uiLabel = UILabel()
+        uiLabel.font = .systemFont(ofSize: 14,weight: .regular)
+        return uiLabel
+    }()
+    
+    /// Shows the price of the item
+    let itemPriceLabel: UILabel = {
+        let uiLabel = UILabel()
+        uiLabel.font = .systemFont(ofSize: 14,weight: .bold)
+        return uiLabel
+    }()
+    
+    /// Add the UI Views
+    func addViews() {
+        
+        // Prevent the view’s autoresizing mask to be translated into
+        // Auto Layout constraints.
+        itemImageView.translatesAutoresizingMaskIntoConstraints = false
+        itemNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        itemPriceLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Add the views to the contentView
+        contentView.addSubview(itemImageView)
+        contentView.addSubview(itemNameLabel)
+        contentView.addSubview(itemPriceLabel)
+        
+        // Dictionary of views to be used in the VFL
+        let viewsDict = [
+            "itemImageView" : itemImageView,
+            "itemNameLabel" : itemNameLabel,
+            "itemPriceLabel" : itemPriceLabel
+        ] as [String : Any]
+        
+        // List of all the constraints
+        var constraints: [NSLayoutConstraint] = []
+        
+        // Vertical Align the itemImageView from the top with height:80
+        let verticalConstraintForItemImageView = NSLayoutConstraint.constraints(withVisualFormat: "V:|-[itemImageView(80)]", options: [], metrics: nil, views: viewsDict)
+        constraints += verticalConstraintForItemImageView
+        
+        // Vertical Align the itemNameLabel 10px from the itemImageView
+        let verticalConstraintForImageAndName = NSLayoutConstraint.constraints(withVisualFormat: "V:[itemImageView(80)]-10-[itemNameLabel(10)]", options: [], metrics: nil, views: viewsDict)
+        constraints += verticalConstraintForImageAndName
+        
+        // Vertical Align the itemPriceLabel 10px from itemNameLabel
+        let verticalConstraintForNameAndPrice = NSLayoutConstraint.constraints(withVisualFormat: "V:[itemNameLabel(10)]-10-[itemPriceLabel(10)]", options: [], metrics: nil, views: viewsDict)
+        constraints += verticalConstraintForNameAndPrice
+        
+        // Horizontal Align the itemImageView to the trailing edge of the superview
+        let horizConstraintImage = NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[itemImageView(80)]", options: [], metrics: nil, views: viewsDict)
+        constraints += horizConstraintImage
+        
+        // Horizontal Align the itemNameLabel to the trailing edge of the superview
+        let horizConstraintName = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[itemNameLabel]", options: [], metrics: nil, views: viewsDict)
+        constraints += horizConstraintName
+        
+        // Horizontal Align the itemPriceLabel to the trailing edge of the superview
+        let horizConstraintPrice = NSLayoutConstraint.constraints(withVisualFormat: "H:|-5-[itemPriceLabel]", options: [], metrics: nil, views: viewsDict)
+        constraints += horizConstraintPrice
+        
+        contentView.addConstraints(constraints)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        addViews()
+    }
 }
--- grocer_store/ProductListingCollectionView/Views/ProductListingCollectionView.swift
+++ grocer_store/ProductListingCollectionView/Views/ProductListingCollectionView.swift
@@ -6,23 +6,112 @@
 //
 
 import UIKit
+import Combine
 
-class ProductListingCollectionView: UIViewController {
+class ProductListingCollectionView: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    private var button: UIButton {
-        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
-        button.setTitle("Test", for: .normal)
-        button.backgroundColor = .black
-        button.setTitleColor(.white, for: .normal)
-        button.center = view.center
-        return button
-    }
-
+    // Defines a cancellable object to retrieve the
+    // state of the Network call.
+    private var cancellable: AnyCancellable?
+    
+    private var viewModel = ViewModel()
+    
+    // Stores the store items
+    private var storeResponse: [Item] = []
+    
+    private var loader: LoaderView!
+    
+    // Label to show an error message if the API fails
+    private var errorLabel: UILabel!
+    
+    var collectionview: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(button)
-        
-        
+        attachViewModelListener()
+        setupLoader()
+        setupErrorLabel()
+        setupCollectionView()
+        fetchData()
     }
-
+    
+    // MARK: Setup Collection View
+    // Setup the Collection View to show the grid of store items.
+    func setupCollectionView() {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        
+        collectionview = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+        collectionview.dataSource = self
+        collectionview.delegate = self
+        collectionview.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
+        collectionview.backgroundColor = .white
+        collectionview.contentInset = UIEdgeInsets(top: 39, left: 20, bottom: 10,right: 0)
+    }
+    
+    // MARK: Setup Loader
+    // Setup the Loader and add it to the subview
+    func setupLoader() {
+        loader = LoaderView(frame: view.frame)
+        view.addSubview(loader.loadingView)
+    }
+    
+    // MARK: Setup Error Label
+    // Setup the Error Label to show any error messages
+    func setupErrorLabel() {
+        errorLabel = UILabel(frame: self.view.frame)
+        errorLabel.text = "Something went wrong, \nPlease try again later"
+        errorLabel.textAlignment = .center
+        errorLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        errorLabel.numberOfLines = 2
+    }
+    
+    // MARK: Fetch Data
+    // Perfrom Network call to fetch Store Data.
+    func fetchData() {
+        Task {
+            try await viewModel.getStoreDetails()
+        }
+    }
+    
+    // MARK: Attach ViewModel Listener
+    // Attach listeners to listen to the Network call
+    // to fetch store data.
+    func attachViewModelListener() {
+        cancellable = viewModel.$store.sink {
+            if($0.isLoading == true) {
+                Task {
+                    self.view.addSubview(self.loader.loadingView)
+                }
+            } else {
+                if($0.success != nil) {
+                    guard let response = $0.success else {return}
+                    self.storeResponse = response.data.items
+                    Task {
+                        self.collectionview.frame = CGRect(x: 0, y: 180, width: self.view.frame.size.width, height: self.view.frame.size.height - 180)
+                        self.view.addSubview(self.collectionview)
+                    }
+                } else if($0.error != "") {
+                    Task {
+                        self.loader.stopLoader()
+                        self.view.addSubview(self.errorLabel)
+                    }
+                }
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        storeResponse.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProductCollectionViewCell.identifier, for: indexPath) as! ProductCollectionViewCell
+        cell.item = storeResponse[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 110, height: 160)
+    }
+    
 }
